9d12b4ded49650d28434f226ea58f407191df3ff
HF-biu <109906241+HF-biu@users.noreply.github.com>
2022-07-29 10:00:15 +0800

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout<<"Hello World\t!\n";
+	cout<< endl;
+	cout<<"Here is 5: "<<5<< "\n";
+    cout<<(float) 5/8<<endl;
+    cout<<8+5<<endl;
+    cout<<sizeof(int)<<endl;
+    
+	return 0;
+ } 
+
